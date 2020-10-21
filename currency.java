8ae0058016bdb6f65ea2c8101000622d7ac8a004
Lalitha Santhoshi Chikkala <52825066+santhoshi-oo9@users.noreply.github.com>
2020-10-21 18:11:56 +0530

    Add files via upload

---
+++ currency.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+         Locale.setDefault(new Locale("en","IN"));
+        String india = NumberFormat.getCurrencyInstance().format(payment);
+        Locale.setDefault(Locale.CHINA);
+        String china = NumberFormat.getCurrencyInstance().format(payment);
+        Locale.setDefault(Locale.US);
+        String us = NumberFormat.getCurrencyInstance().format(payment);
+        Locale.setDefault(Locale.FRANCE);
+        String france = NumberFormat.getCurrencyInstance().format(payment);
+        
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
